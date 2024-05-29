3c249af81e9f5e442c2674617f8ef39e92bbbad8
AnanymousAch <35920575+AnanymousAch@users.noreply.github.com>
2024-05-29 10:37:58 +0545

    counter button

--- React/part1/src/counter.jsx
+++ React/part1/src/counter.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react"
 
-const App = (props) => {
-    const { counter } = props
+const App = () => {
+    const [counter, setCounter] = useState(0)
+
+    const handleClick = () => {
+        console.log('clicked')
+    }
+
+    // setTimeout(
+    //     () => setCounter(counter + 1), 1000
+    // )
+
+    // console.log('rendering...', counter)
+
     return (
         <div>
-            {counter}
+        {counter}
+            <br></br>
+            <button onClick={() => setCounter(counter + 1)}>
+                plus
+            </button>
+            <button onClick={() => setCounter(0)}>
+                Zero
+            </button>
         </div>
     )
 }
--- React/part1/src/main.jsx
+++ React/part1/src/main.jsx
@@ -1,17 +1,5 @@
-import ReactDOM from 'react-dom/client'
+import ReactDom from 'react-dom/client'
 
 import App from './counter'
 
-let counter = 1
-const refresh = () => {
-
-    ReactDOM.createRoot(document.getElementById('root')).render(
-        <App counter={counter} />
-    )
-
-}
-
-setInterval(() => {
-    refresh()
-    counter += 1
-}, 1000)
+ReactDom.createRoot(document.getElementById('root')).render(<App />)
