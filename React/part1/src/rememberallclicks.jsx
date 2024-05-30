c98307f88044e3852fb94c3f13afa2874b956e0a
AnanymousAch <35920575+AnanymousAch@users.noreply.github.com>
2024-05-30 10:37:02 +0545

    some messages added to make code understandable

--- React/part1/src/rememberallclicks.jsx
+++ React/part1/src/rememberallclicks.jsx
@@ -1,5 +1,34 @@
 import { useState } from "react";
 
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used by pressing the buttons
+            </div>
+        )
+    }
+    return (
+        <div>
+            button press history:{props.allClicks.join('')}
+        </div>
+
+    )
+}
+
+const Button = ({ handleClick, text }) => (
+    //here retun is not used because it is single line ccode
+    //and is written between (these brackets)
+    //if {curly brackets} were used we hadd no option but to use return to
+    //make the button display
+    <button onClick={handleClick}>
+        {text}
+    </button>
+
+
+)
+
+
 const App = () => {
     const [left, setLeft] = useState(0)
     const [right, setRight] = useState(0)
@@ -25,12 +54,12 @@ const App = () => {
     return (
         <div>
             {left}
-            <button onClick={handleLeftClick}>left</button>
-            <button onClick={handleRightClick}>right</button>
+            <Button handleClick={handleLeftClick} text='left' />
+            <Button handleClick={handleRightClick} text='Right' />
             {right}
-            <p>Total {total} </p>
+            <p>Total: {total}</p>
 
-            <p>{allClicks.join(' ')}</p>
+            <History allClicks={allClicks} />
         </div>
     )
 }
