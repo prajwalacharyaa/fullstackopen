16a4ce0d704f78376c9967f6cea6544550176300
AnanymousAch <35920575+AnanymousAch@users.noreply.github.com>
2024-05-30 10:08:24 +0545

    remember all clicks

--- React/part1/src/rememberallclicks.jsx
+++ React/part1/src/rememberallclicks.jsx
@@ -3,19 +3,23 @@ import { useState } from "react";
 const App = () => {
     const [left, setLeft] = useState(0)
     const [right, setRight] = useState(0)
-
     const [allClicks, setAll] = useState([])
-
+    const [total, setTotal] = useState(0)
 
     const handleLeftClick = () => {
         setAll(allClicks.concat('L'))
-        setLeft(left + 1)
+        //   console.log('Left before', left)
+        const updatedLeft = left + 1
+        setLeft(updatedLeft)
+        // console.log('left after', left)
+        setTotal(updatedLeft + right)
     }
-
 
     const handleRightClick = () => {
         setAll(allClicks.concat('R'))
-        setRight(right + 1)
+        const updatedright = right + 1
+        setRight(updatedright)
+        setTotal(left + updatedright)
     }
 
     return (
@@ -24,6 +28,7 @@ const App = () => {
             <button onClick={handleLeftClick}>left</button>
             <button onClick={handleRightClick}>right</button>
             {right}
+            <p>Total {total} </p>
 
             <p>{allClicks.join(' ')}</p>
         </div>
